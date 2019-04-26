34ad1ae4d589c3372e437015a5576b58b47c9354
Limfips Limfips <limfips7@gmail.com>
2019-04-26 05:25:27 +0400

    v1.1 Начал дизайн делать

--- WindowsApp/MainWindow.xaml.cs
+++ WindowsApp/MainWindow.xaml.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Media3D;
 using WindowsApp.configs;
+using MaterialDesignThemes.Wpf;
 
 namespace WindowsApp
 {
@@ -10,32 +15,87 @@ namespace WindowsApp
         public MainWindow()
         {
             InitializeComponent();
+            ListBox.ItemsSource = GetFileNames();
         }
 
         private void MenuItemExit_OnClick(object sender, RoutedEventArgs e)
         {
             Environment.Exit(0);
         }
-        private void GetFiles_OnClick(object sender, RoutedEventArgs e)
+        
+        private void MenuItemCreateTimetable_OnClick(object sender, RoutedEventArgs e)
         {
-            ListBox.Items.Clear();
+            MainGrid.Visibility = Visibility.Collapsed;
+            UserNewTimetableGrid.Visibility = Visibility.Visible;
+        }
+        
+        private void MenuItemReturnMainGrid_OnClick(object sender, RoutedEventArgs e)
+        {
+            UserNewTimetableGrid.Visibility = Visibility.Collapsed;
+            OpenUserTimetableGrid.Visibility = Visibility.Collapsed;
+            MainGrid.Visibility = Visibility.Visible;
+        }
+        private void MenuItemOpenTimetable_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenTimetable();
+        }
+        private void MouseButtonOpenTimetable_OnClick(object sender, MouseButtonEventArgs e)
+        {
+            OpenTimetable();
+        }
+
+        private void OpenTimetable()
+        {
+            MainGrid.Visibility = Visibility.Collapsed;
+            OpenUserTimetableGrid.Visibility = Visibility.Visible;
+            UserTimetableTextBlock.Text = (string) ListBox.SelectedItems[0];
+
+            string newPatch = $"c:\\WindowsAppDir\\{(string) ListBox.SelectedItems[0]}.txt";
+            var fileR = new StreamReader(newPatch);
+            string fileText = "";
+            string line;
+            while ((line = fileR.ReadLine()) != null)
+            {
+                fileText += line;
+            }
+            fileR.Close();
+            Card asd = new Card
+            {
+                Content = fileText,
+                Margin = new Thickness(0,1,0,0),
+                Padding = new Thickness(5)
+                
+            };
+            Grid.Children.Add(asd);
+        }
+
+        private List<string> GetFileNames()
+        {
             var dirPath = "c:\\WindowsAppDir";
             var filePaths = Directory.GetFiles(dirPath);
-            foreach (var filePath in filePaths)
-            {
-                ListBox.Items.Add(Path.GetFileNameWithoutExtension(filePath));
-            }
+            return filePaths.Select(Path.GetFileNameWithoutExtension).ToList();
+        }
+
+        private void GetFiles_OnClick(object sender, RoutedEventArgs e)
+        {
+            ListBox.ItemsSource = GetFileNames();
             //<ListBox  x:Name="ListBox" MouseDoubleClick="GetSelectedItem"/>
         }
-//        private void GetSelectedItem(object sender, MouseButtonEventArgs e)
-//        {
-//            string book = (string) ListBox.SelectedItems[0];
-//            string newPatch = $"c:\\WindowsAppDir\\{book}.txt";
-//            StreamWriter fileW = new StreamWriter(newPatch);
-//            fileW.WriteLine("А я тыкал по нему)))\n");
-//            fileW.Close();
-//            var fileR = new StreamReader(newPatch);
-//            TextBlock.Text = fileR.ReadLine();
-//        }
+
+        private void MenuItemRenameTimetable_OnClick(object sender, RoutedEventArgs e)
+        {
+            string newPatch = $"c:\\WindowsAppDir\\{(string) ListBox.SelectedItems[0]}.txt";
+            File.Move(newPatch, "c:\\WindowsAppDir\\newfilename.txt");
+            GetFiles_OnClick(sender,e);
+        }
+
+        private void MenuItemDeleteTimetable_OnClick(object sender, RoutedEventArgs e)
+        {
+            string newPatch = $"c:\\WindowsAppDir\\{(string) ListBox.SelectedItems[0]}.txt";
+            File.Delete(newPatch);
+            GetFiles_OnClick(sender,e);
+        }
     }
+    
+    
 }
